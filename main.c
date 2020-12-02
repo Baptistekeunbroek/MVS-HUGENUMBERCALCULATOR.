a26ac461cb1118411d90238ec29d25dc2381619f
thibault0 <73802041+thibault0@users.noreply.github.com>
2020-12-02 13:21:13 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,9 @@
+#include "UseHugeNumber/Operator/HugeFloatOperator.h"
+
+#include "UseHugeNumber/Operator/HugeIntOperator.h"
+
+#include "UseHugeNumber/Operator/HugeUnsignedIntOperator.h"
+
 int main (void) {
  HugeUnsignedInt* hugeNumber = createHugeUnsignedIntFromString
 ("123456789012345678901234567890");
